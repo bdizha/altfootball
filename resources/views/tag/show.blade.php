ff9097b07abe38d793d001042cfcc70bcdaeec14
Batanayi Matuku <bdizha@gmail.com>
2017-09-30 22:35:29 +0200

    Modified: tag show layout

--- resources/views/tag/show.blade.php
+++ resources/views/tag/show.blade.php
@@ -8,18 +8,15 @@
     <div class="_3LeFV">
         <h1 class="_35Eoh">#{{ $tag->content }}</h1>
         <h2 class="_1Honn">The best {{ $tag->content }} content on ALTFOOTBALL</h2>
-        <div class="_2cfq3"><span class="_3OQAJ">Join ALTFOOTBALL to get posts directly to your inbox</span>
-            <div class="_1KXd6"><a href="/signin" class="_2LrKA KyQLW">With email</a><span class="_1iBcU">Or</span>
-                <a href="https://www.facebook.com/v2.9/dialog/oauth?client_id=516295178554349&amp;redirect_uri=https://drivetribe.com/fb-sign-in&amp;response_type=token&amp;scope=public_profile, email"
-                   class="_1S4og KyQLW">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 18 18">
-                        <path fill="#FEFEFE" fill-rule="evenodd"
-                              d="M17 0H1a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1H9.6v-6.97H7.27V8.3H9.6v-2c0-2.33 1.42-3.6 3.5-3.6 1 0 1.85.08 2.1.12v2.43h-1.44c-1.13 0-1.35.53-1.35 1.32V8.3h2.69l-.35 2.72h-2.34V18h4.59a1 1 0 0 0 .99-1V1a1 1 0 0 0-1-1"></path>
-                    </svg>
-                    With Facebook
-                </a>
+        @if(!Auth::guard()->check())
+            <div class="_2cfq3"><span class="_3OQAJ">Join ALTFOOTBALL to get posts directly to your inbox</span>
+                <div class="_1KXd6">
+                    <a href="/register" class="_1ssnS _35Ns5">Sign in with Email</a>
+                    <span class="_1iBcU">Or</span>
+                    <a href="/fb" class="MAYYh _35Ns5">Sign in with Facebook</a>
+                </div>
             </div>
-        </div>
+        @endif
         <div class="_26DkX">
             <h3 class="_3MbTo">Top Bases</h3>
             <div class="_1SVpA _2QjgM">
@@ -78,4 +75,7 @@
             </div>
         </div>
     </div>
+    @include('templates.dribbles')
+    @include('templates.comments')
+    @include('templates.posts')
 @endsection
