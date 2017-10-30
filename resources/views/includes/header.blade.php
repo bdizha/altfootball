02e8743e912b975bc235d8fd468b34284c7abd13
Batanayi Matuku <bdizha@gmail.com>
2017-10-30 22:14:47 +0200

    Modified: layout

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,21 +5,6 @@
             <a class="" href="/">
                 <img src="/images/text.png" class="_5YHG" title="AltFootball" alt="AltFootball" />
             </a>
-            <form class="ra77F" action="">
-                <div class="_1YqNB _1eF7g _3TYH2">
-                    <input type="search" value=""
-                           placeholder="Search AltFootball"
-                           class="_3vbhf l4oHd"
-                    >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="42"
-                         viewBox="0 0 40 42" class="_2-OFE _1_FTk _3ZPAz">
-                        <path fill="#00AFFF" fill-rule="evenodd"
-                              d="M34.2 37.01l-7.96-7.88c.56-.43 1.1-.89 1.6-1.4l.14-.15 7.87 7.78-1.64 1.65zM7.25 25.4a12.76 12.76 0 0 1-3.8-9.06 12.75 12.75 0 0 1 3.71-9.1 12.77 12.77 0 0 1 9.07-3.8h.06c3.4 0 6.62 1.32 9.04 3.72a12.76 12.76 0 0 1 3.8 9.06A12.86 12.86 0 0 1 7.24 25.4zm32.25 8.73l-9.39-9.28a16.14 16.14 0 0 0 2.45-8.64 16.17 16.17 0 0 0-4.81-11.5A16.17 16.17 0 0 0 16.28 0h-.07A16.17 16.17 0 0 0 4.7 4.82 16.17 16.17 0 0 0 0 16.35a16.18 16.18 0 0 0 4.82 11.5 16.18 16.18 0 0 0 11.46 4.7h.07A16.2 16.2 0 0 0 23.23 31L33 40.66a1.71 1.71 0 0 0 2.43-.01l4.06-4.1a1.72 1.72 0 0 0-.01-2.42z"
-                        ></path>
-                    </svg>
-                </div>
-                <button type="reset" class="_1C5MC">Cancel</button>
-            </form>
         </div>
         <div class="_3BBJg">
             @if(Auth::guard()->check() && Auth::user()->is_active)
