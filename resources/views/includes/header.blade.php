d8a8f8171ac55e89ac0b18ac00640a514980d0df
Batanayi Matuku <bdizha@gmail.com>
2017-11-11 18:13:02 +0200

    Modified: layout

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,13 @@
-
 <header class="_3XXLi">
-    <nav class="R1XAV" id="header">
+    <nav class="R1XAV jMZSBo" id="header">
+        <div class="sc-kAzzGY fVSfy">
+            <div class="sc-ckVGcZ bUebbF">
+                <a class="sc-brqgnP hBQeWD" href="/fanbases">Fanbases</a>
+            </div>
+        </div>
         <div class="_2H55B">
-            <a class="" href="/">
-                <img src="/images/text.png" class="_5YHG" title="AltFootball" alt="AltFootball" />
+            <a class="sc-kgoBCf jHUdQQ" href="/">
+                <span class="dOqZCB"><span style="color: #0095D0">Alt</span>Football</span>
             </a>
         </div>
         <div class="_3BBJg">
@@ -12,8 +16,10 @@
                 <button class="_13sKJ">
                     <span class="T3zZE">Alerts</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="18" viewBox="0 0 16 18" class="_8e7Ip">
-                        <g fill="none" fill-rule="evenodd" stroke="#00AFFF" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke-width="1.5" d="M13.85 9.37v2.26l1 2.34H.75l.97-2.37V6.81a6.06 6.06 0 0 1 12.13 0v2.56z"></path>
+                        <g fill="none" fill-rule="evenodd" stroke="#00AFFF" stroke-linecap="round"
+                           stroke-linejoin="round">
+                            <path stroke-width="1.5"
+                                  d="M13.85 9.37v2.26l1 2.34H.75l.97-2.37V6.81a6.06 6.06 0 0 1 12.13 0v2.56z"></path>
                             <path d="M10.76 14.3L7.8 17.25l-2.94-2.94"></path>
                         </g>
                     </svg>
@@ -23,7 +29,8 @@
                     <div class="_25jNX X4CNn">
                         <div class="N3r_f">
                             <div class="_38L6D" style="padding-bottom: 100%;">
-                                <img alt="" role="presentation" src="{{ $user->thumb_x }}" class="_214e9 b00q8" width="60" height="60">
+                                <img alt="" role="presentation" src="{{ $user->thumb_x }}" class="_214e9 b00q8"
+                                     width="60" height="60">
                             </div>
                         </div>
                     </div>
@@ -35,7 +42,7 @@
         </div>
     </nav>
     <div class="_3GUPV -hkpO">
-        <div class="_2Ruh1"style="opacity: 0; transform: translateY(-75px) translateZ(-40px);">
+        <div class="_2Ruh1" style="opacity: 0; transform: translateY(-75px) translateZ(-40px);">
             <nav class="_3GkHt _3AKLf">
                 <button class="zxDoM undefined"><!-- react-text: 32 -->Activity
                     <!-- /react-text --><!-- react-empty: 33 --></button>
--- resources/views/templates/posts.blade.php
+++ resources/views/templates/posts.blade.php
@@ -9,7 +9,7 @@
                             <span data-bind="text: '@' + fanbase.slug"></span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 18 18" class="G0duV">
                                 <g fill="none" fill-rule="evenodd">
-                                    <path fill="#57d100" d="M0 9a9 9 0 1 0 18 0A9 9 0 0 0 0 9z"></path>
+                                    <path fill="#33BB66" d="M0 9a9 9 0 1 0 18 0A9 9 0 0 0 0 9z"></path>
                                     <path fill="#FFF" d="M12.38 5.17l1.58 1.58-6.09 6.08L4.04 9l1.58-1.58 2.25 2.25"></path>
                                 </g>
                             </svg>
