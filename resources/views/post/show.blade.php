f1c0997072136c7937ca393ab526af9596b21a83
Batanayi Matuku <bdizha@gmail.com>
2018-05-01 00:35:57 +0200

    Modified: post look n feel

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -17,17 +17,17 @@
                                 <div class="_TY876">
                                     <div class="_2H69I">UP NEXT_</div>
                                     <ol class="_FGT65">
-                                        @foreach($posts['trending'] as $k => $post)
+                                        @foreach($posts['trending'] as $k => $p)
                                             <li class="_89GHT">
                                                 <div class="_CFG34">
-                                                    <a class="_CFR45" href="/u/{{ $post->user->slug }}">
-                                                        <img alt="{{ $post->user->name }}" role="presentation" src="{{ $post->user->small_x }}"
+                                                    <a class="_CFR45" href="/p/{{ $p->slug }}">
+                                                        <img alt="{{ $p->title }}" role="presentation" src="{{ $p->thumb_x }}"
                                                              class="_214e9 b00q8">
                                                     </a>
                                                 </div>
                                                 <div class="_56KHY">
-                                                    <a class="_GJ585" href="/p/{{ $post->slug }}">
-                                                        <h3 class="_23GTY">{{ $post->title }}</h3>
+                                                    <a class="_GJ585" href="/p/{{ $p->slug }}">
+                                                        <h3 class="_23GTY">{{ $p->title }}</h3>
                                                     </a>
                                                     <div class="_ETY90">
                                                         <div class="_89RTY">
@@ -36,14 +36,14 @@
                                                                     <div class="_98HTY">
                                                                         <span>Posted in</span>
                                                                         <a class="_GTYKY"
-                                                                           href="/f/{{ $post->fanbase->slug }}">{{ $post->fanbase->camel }}</a>
+                                                                           href="/f/{{ $p->fanbase->slug }}">{{ $p->fanbase->camel }}</a>
                                                                     </div>
                                                                 </div>
                                                                 <div class="_YURM4">
                                                                     <div class="_TYMN3">
-                                                                        <time class="_FVT43">{{ $post->published_at }}</time>
+                                                                        <time class="_FVT43">{{ $p->published_at }}</time>
                                                                         <span class="_D45RT"></span>
-                                                                        <span class="_23FRT">{{ $post->reading_time }}</span>
+                                                                        <span class="_23FRT">{{ $p->reading_time }}</span>
                                                                     </div>
                                                                 </div>
                                                             </div>
--- resources/views/post/siblings.blade.php
+++ resources/views/post/siblings.blade.php
@@ -1,17 +1,17 @@
 @foreach($range as $key)
     <?php if(empty($posts["sibling"][$key])) continue; ?>
-    <?php $post = $posts["sibling"][$key] ?>
+    <?php $p = $posts["sibling"][$key] ?>
     <div class="_1XEbE">
         <div class="_1DRo">
             <div class="_38L6D" >
-                <img alt="{{ $post->title }}" role="presentation" src="{{ $post->small_x }}"
+                <img alt="{{ $p->title }}" role="presentation" src="{{ $p->small_x }}"
                      class="_214e9 b00q8">
             </div>
         </div>
         <div class="_1wjeD">
-            <div class="grsTy">{{ $post->title }}</div>
+            <div class="grsTy">{{ $p->title }}</div>
         </div>
-        <a class="ZE8ka" href="/p/{{ $post->slug }}"></a>
+        <a class="ZE8ka" href="/p/{{ $p->slug }}"></a>
         @include('post.user')
     </div>
 @endforeach
